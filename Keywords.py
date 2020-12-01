4791e874fc779d4a6ab4656cc90a40733d783466
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-12-01 13:53:32 -0500

    all test passing expect PTO apostrophe

--- Keywords.py
+++ Keywords.py
@@ -33,7 +33,6 @@ class Keywords:
             formula = "{0: <30}\t{1: <30}\t= \tmath.log(({2} * 2) / {3} )".format(
                 token, rank, current_token_frequency, current_corpus_frequency
             )
-            print(f"adding: {formula}")
             self.__scores[token] = formula
             token_ranks[token] = rank
             sorted_token_ranks = sorted(token_ranks, key=token_ranks.get, reverse=True)
